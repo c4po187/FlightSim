2713197cd740d24b5176509a287f1f7aab033633
c4po187 <c4po187@gmail.com>
2015-08-28 05:08:21 +0100

    CHANGELOG
    - Minor change

--- Transform.cpp
+++ Transform.cpp
@@ -32,32 +32,17 @@ Transform::Transform() :
 Transform::~Transform() { ; }
 
 ModelTransform::ModelTransform(const Vec3& position, const Vec3& direction,
-	const Vec3& up, const std::string& tag) {
-	m_tag = tag;
-	m_position = position;
-	m_direction = direction;
-	m_up = up;
-	m_orientation = Quatn();
-	m_yaw = m_pitch = m_roll = .0f;
+	const Vec3& up, const std::string& tag) :
+	Transform(position, direction, up, tag) {
 	m_modelMatrix = Matrix4(1.0f);
 }
 
-ModelTransform::ModelTransform(const Vec3& position, const std::string& tag) {
-	m_tag = tag;
-	m_position = position;
-	m_direction = Vec3(.0f, .0f, -1.0f);
-	m_up = Vec3(.0f, 1.0f, .0f);
-	m_orientation = Quatn();
-	m_yaw = m_pitch = m_roll = .0f;
+ModelTransform::ModelTransform(const Vec3& position, const std::string& tag) :
+	Transform(position, tag) {
 	m_modelMatrix = Matrix4(1.0f);
 }
 
-ModelTransform::ModelTransform() {
-	m_position = Vec3();
-	m_direction = Vec3(.0f, .0f, -1.0f);
-	m_up = Vec3(.0f, 1.0f, .0f);
-	m_orientation = Quatn();
-	m_yaw = m_pitch = m_roll = .0f;
+ModelTransform::ModelTransform() : Transform() {
 	m_modelMatrix = Matrix4(1.0f);
 }
 
